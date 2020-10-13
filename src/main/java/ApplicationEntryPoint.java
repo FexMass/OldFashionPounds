feaa7dad34400d08da956cab0de597de3c5bea53
FexMass <fertz1993@gmail.com>
2020-10-14 00:49:13 +0200

    Added logic and validation for hndling input and Class CurrencyConverter

--- src/main/java/ApplicationEntryPoint.java
+++ src/main/java/ApplicationEntryPoint.java
@@ -28,7 +28,34 @@ public class ApplicationEntryPoint {
      * @param input Input provide by user
      */
     public static String handleInput(String input) {
+        try {
+            String[] splitNumbers = input.split("\\s+");
 
+            //Validate input
+            if (!(splitNumbers.length == 5 || splitNumbers.length == 7)) {
+                return ("Format is wrong");
+            }
+            if (!splitNumbers[3].matches("[+*-/]")) {
+                return ("Unknown operator used");
+            }
+
+            //Input format 1 for (Xp Ys Zd +- Xp Ys Zd)
+            if (splitNumbers.length == 7 && splitNumbers[3].matches("[+-]")) {
+                Amount amount1 = new Amount(splitNumbers[0], splitNumbers[1], splitNumbers[2]);
+                Amount amount2 = new Amount(splitNumbers[4], splitNumbers[5], splitNumbers[6]);
+
+                return splitNumbers[3].equals("+") ? CurrencyConverter.sum(amount1, amount2) :
+                        CurrencyConverter.subtract(amount1, amount2);
+            }
+            //Input format 2 for (Xp Ys Zd */ NUM)
+            if (splitNumbers.length == 5 && splitNumbers[3].matches("[*/]")) {
+                Amount amount1 = new Amount(splitNumbers[0], splitNumbers[1], splitNumbers[2]);
+                return splitNumbers[3].equals("*") ? CurrencyConverter.multiply(amount1, Integer.parseInt(splitNumbers[4])) :
+                        CurrencyConverter.divide(amount1, Integer.parseInt(splitNumbers[4]));
+            }
+        } catch (NumberFormatException ex) {
+            return "Exception occurred: " + ex.getMessage();
+        }
         return "Unknown input!";
     }
 }
