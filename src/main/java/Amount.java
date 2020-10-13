d32f0fef5f3730bc69537583fb1648c9e5e331b6
FexMass <fertz1993@gmail.com>
2020-10-14 00:18:52 +0200

    Added model Amount representing pounds, shillings and pence

---
+++ src/main/java/Amount.java
@@ -0,0 +1,44 @@
+/**
+ * Model containing input values to be calculated which are filled by user with extra validation
+ */
+public class Amount {
+
+    private final int pounds;
+    private final int shillings;
+    private final int pence;
+
+    public Amount(String pounds, String shillings, String pence) {
+        if (!pounds.endsWith("p") || !shillings.endsWith("s") || !pence.endsWith("d")) {
+            throw new IllegalArgumentException();
+        }
+        this.pounds = Integer.parseInt(pounds.replace("p", ""));
+        this.shillings = Integer.parseInt(shillings.replace("s", ""));
+        this.pence = Integer.parseInt(pence.replace("d", ""));
+    }
+
+    public Amount(int pounds, int shillings, int pence) {
+        this.pounds = pounds;
+        this.shillings = shillings;
+        this.pence = pence;
+    }
+
+    public int getPounds() {
+        return pounds;
+    }
+
+    public int getShillings() {
+        return shillings;
+    }
+
+    public int getPence() {
+        return pence;
+    }
+
+    @Override
+    public String toString() {
+        return  "Result: " +
+                pounds + "p " +
+                shillings + "s " +
+                pence + "d";
+    }
+}
